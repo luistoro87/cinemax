fbd64ae1842de723c5283d132563f01eecdb6401
Pedro Luis Toro Acosta <pedro.toro@onoff.com.co>
2023-10-21 22:18:18 -0500

    implemened navbar sacial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { NavbarSocialComponent } from './landing-page/navbar-social/navbar-social.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    NavbarSocialComponent
   ],
   imports: [
     BrowserModule,
